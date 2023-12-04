a4eab6fece5570086273062e268608993631349b
jkpate11 <jkpate11@asu.edu>
2023-12-04 00:41:47 -0700

    change

--- src/App.js
+++ src/App.js
@@ -7,12 +7,55 @@ function App() {
   const [tableHeaders, setTableHeaders] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 5;
-  const URL = `http://54.200.226.251:3030/sample/query`;
+  const URL = `http://54.191.86.183:3030/sample/query`;
 
-  const handleQuery = async (e) => {
+  const queries = [
+    'PREFIX homicide: <http://www.semanticweb.org/dvadher1/ontologies/2023/10/homicide-tracker-and-analyser#> \
+    SELECT (COUNT(?offender) AS ?numGuns) \
+    WHERE { \
+      ?offender a homicide:Offender ; \
+                 homicide:hasWeapon ?weapon . \
+      FILTER(?weapon = 11 || ?weapon = 12 || ?weapon = 13 || ?weapon = 14 || ?weapon = 15) \
+    }',
+    'PREFIX homicide: <http://www.semanticweb.org/dvadher1/ontologies/2023/10/homicide-tracker-and-analyser#> \
+    SELECT ?incident ?offender ?victim \
+    WHERE { \
+      ?incident a homicide:Incident ; \
+                 homicide:hasHomMult ?homMult . \
+      FILTER(str(?homMult) = "D") \
+      ?incident homicide:involvesOffender ?offender . \
+      ?incident homicide:involvesVictim ?victim .  \
+    }',
+    'PREFIX homicide: <http://www.semanticweb.org/dvadher1/ontologies/2023/10/homicide-tracker-and-analyser#> \
+    SELECT ?month (COUNT(?incident) AS ?numIncidents) \
+    WHERE { \
+      ?incident a homicide:Incident ; \
+                homicide:hasMonth ?rawMonth . \
+      BIND(REPLACE(?rawMonth,"^0*","") AS ?month) \
+    } \
+    GROUP BY ?month \
+    ORDER BY ?month',
+    'PREFIX homicide: <http://www.semanticweb.org/dvadher1/ontologies/2023/10/homicide-tracker-and-analyser#> \
+    SELECT ?state (COUNT(?incident) AS ?numIncidents) \
+    WHERE { \
+      ?incident a homicide:Incident ; \
+                homicide:hasState ?state. \
+    } \
+    GROUP BY ?state \
+    ORDER BY ?state',
+    'PREFIX homicide: <http://www.semanticweb.org/dvadher1/ontologies/2023/10/homicide-tracker-and-analyser#> \
+    SELECT ?offender \
+    WHERE { \
+      ?offender a homicide:Offender ; \
+       homicide:hasRace ?o1race . \
+       FILTER(?o1race = "B") \
+    }'
+  ];
+
+  const handleQuery = async (e, query) => {
     e.preventDefault();
+    queryRef.current.value = query;
 
-    const query = queryRef.current.value;
     const queryFinal = { query: query };
 
     var formBody = [];
@@ -45,6 +88,14 @@ function App() {
       setTableHeaders([]);
       setTableData([]);
     }
+  };
+
+  const renderQueryButtons = () => {
+    return queries.map((query, index) => (
+      <button key={index} onClick={(e) => handleQuery(e, query)}>
+        Query {index + 1}
+      </button>
+    ));
   };
 
   const renderTable = () => {
@@ -87,7 +138,7 @@ function App() {
     }
 
     return (
-      <div>
+      <div className="pagination">
         <button
           disabled={currentPage === 1}
           onClick={() => setCurrentPage(currentPage - 1)}
@@ -108,8 +159,8 @@ function App() {
   return (
     <div className="container">
       <div>
-      <h1 className="title">Homicide Tracker and Analyzer</h1>
-        <form className="custom-form" onSubmit={handleQuery}>
+        <h1 className="title">Homicide Tracker and Analyzer</h1>
+        <form className="custom-form" onSubmit={(e) => handleQuery(e, queryRef.current.value)}>
           <textarea
             className="custom-textarea"
             ref={queryRef}
@@ -121,6 +172,10 @@ function App() {
             Submit
           </button>
         </form>
+
+        <div className="query-buttons">
+          {renderQueryButtons()}
+        </div>
 
         <div className="table-container">
           <h2>Result:</h2>
